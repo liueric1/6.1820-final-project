4d57dc37afff2f8d1e1f4c5873cfb43077f19511
Eric Liu <liueric@mit.edu>
2025-02-05 14:59:55 -0500

    Initial Commit

---
+++ lab0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab0
+//
+//  Created by Eric Liu on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
