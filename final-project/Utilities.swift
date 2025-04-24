98db898d18089b83eb755ddce817193040600fbd
Anna Murphy <almurph@mit.edu>
2025-04-23 23:01:56 -0400

    reorganize files, add Leticia's functions

---
+++ final-project/Utilities.swift
@@ -0,0 +1,87 @@
+//
+//  Utilities.swift
+//  final-project
+//
+//  Created by Anna Murphy on 4/23/25.
+//
+
+import UIKit
+import Foundation
+import AVFoundation
+import Accelerate
+
+extension Recording {
+    
+    func shift(fft: [[Double]]) -> [[Double]] {
+        print(fft[0][0], fft[1][0])
+        print(fft[0][1], fft[1][1])
+        print(fft[0][2], fft[1][2])
+        let columnCount = fft.first!.count
+        let offset = columnCount / 2
+
+        return fft.map { row in
+            Array(row[(columnCount - offset)...] + row[..<(columnCount - offset)])
+        }
+    }
+    
+    func applyArgmax(matrix: [[Double]]) -> [Int] {
+        return matrix.map { row in
+            row.indices.max(by: { row[$0] < row[$1] })!
+        }
+    }
+    
+    func median(array: [Int]) -> Double {
+        let sorted = array.sorted()
+        if sorted.count % 2 == 0 {
+            return Double((sorted[(sorted.count / 2)] + sorted[(sorted.count / 2) - 1])) / 2
+        }
+        else {
+            return Double(sorted[(sorted.count - 1) / 2])
+        }
+    }
+    
+    func getColumns(matrix: [[Double]], columnIndices: [Int]) -> [[Double]] {
+        return matrix.map { row in
+            return columnIndices.map { idx in
+                return row[idx]
+            }
+        }
+    }
+    
+    func getDistanceFromPeak(idx: Int, windowRangeStart: Int, medianPeakLocation: Int) -> Int {
+        return idx + windowRangeStart - medianPeakLocation
+    }
+    
+    func indxToDistance(idx: Int, windowLength: Int, sampleRate: Double, freqHigh: Double, freqLow: Double, chirpLength: Double) -> Double {
+        let speedSound: Double = 343
+        let top: Double = ((Double(idx) *  Double(windowLength) / sampleRate) * speedSound)
+        let bottom: Double = (2 * ((freqHigh - freqLow) / chirpLength))
+        return top / bottom
+    }
+    
+    func medianFilter(array: [Int], size: Int) -> [Int] {
+        let pad = size / 2
+        let padded = Array(repeating: array.first, count: pad)
+            + array
+            + Array(repeating: array.last, count: pad)
+
+        let rolled = pad..<(padded.count - pad)
+        return rolled.map { i in
+            let window = padded[(i - pad)...(i + pad)]
+            let sortedWindow = window.compactMap{ $0 }.sorted()
+            return sortedWindow[pad]
+        }
+    }
+    
+    func movingAverage(array: [Double], size: Int) -> [Double] {
+        var result: [Double] = []
+        var windowSum = array.prefix(size).reduce(0, +)
+        result.append(windowSum / Double(size))
+
+        for i in size..<array.count {
+            windowSum += array[i] - array[i - size]
+            result.append(windowSum / Double(size))
+        }
+        return result
+    }
+}
--- final-project/ViewController.swift
+++ final-project/ViewController.swift
@@ -24,9 +24,12 @@ class ViewController: UIViewController {
         recording.generateTone(startFrequency: 440, endFrequency: 880, chirp_duration: 0.05, total_duration: 5)
         recording.start()
         
-        // TEST FFT & BUTTER FUNCTIONS
-//        let fftMagnitudes = recording.testFFTFromJSON()
-//        recording.saveFFTResultToDocumentsAndShare(fftMagnitudes, filename: "fft_output.json", presentingViewController: self)
+        // TEST FFT
+//        let fftMagnitudes = recording.testFFTsFromMultiChirpJSON(txFilename: "tx_list", rxFilename: "rx_list")
+//        recording.saveFFTResultToDocumentsAndShare(fftMagnitudes, filename: "fft_output_2.json", presentingViewController: self)
+//        
+//        let subtracted = recording.backgroundSubtraction(allMultipliedFfts: fftMagnitudes)
+//        recording.saveFFTResultToDocumentsAndShare(subtracted, filename: "subtracted.json", presentingViewController: self)
         
         Timer.scheduledTimer(withTimeInterval: recordingDuration, repeats: false) { [weak self] _ in
                 guard let self = self else { return }
